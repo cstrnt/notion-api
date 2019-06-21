d524d175eeafe7d6d57d7d4d0b9d514641d31c62
Tim Raderschad <tiraderschad@conet.de>
2019-06-21 16:49:25 +0200

    ignore dist

--- dist/lib/fetch.d.ts
+++
@@ -1,8 +0,0 @@
-declare function request({ endpoint, creds: { token }, body }: {
-    endpoint: string;
-    creds: {
-        token: string;
-    };
-    body?: object;
-}): Promise<any>;
-export default request;
