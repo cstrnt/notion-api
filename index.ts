57601b1c488011797cae87309da3d9a55b7d4ec5
Tim Raderschad <tim.raderschad@gmail.com>
2019-09-04 08:41:32 +0200

    Update index.ts

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import Notion from './src/notion';
 
 const options = {
   token_v2:
-    '828227322c2c14e874045a4e38971bb3b81e06e6f4d030ef806ddb6c61720cc973c099fffef9c297e1db49a03a1d7ab757d2854d40861cd982b6431465c1bb7fcc2b0f87ecaaa66d3ea758e44315'
+    '<Enter Token Here>'
 };
 
 const api = new Notion({
